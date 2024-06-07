1dbd26cff29268afaa8f63a6804dafea8071a752
MARIA CLARA RODRIGUES SILVA <mariaclara.rodrigues@a.ucb.br>
2024-06-07 11:01:48 -0300

    Preenchendo os dados do formulario

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,6 +1,16 @@
 describe('Teste Cypress', () => {
   it('Acesso ao site sauce demo', () => {
     cy.visit('https://phptravels.org/register.php')
-    cy.get("#inputFirstName").type('');
+    cy.get("#inputFirstName").type('Maria');
+    cy.get('#inputLastName').type('Silva');
+    cy.get('#inputEmail').type('maria123456789@gmail.com');
+    cy.get('#inputAddress1').type('catolica')
+    cy.get('#inputCity').type('bsb')
+    cy.get('#inputPhone').type('61900000000')
+    cy.get('#inputNewPassword1').type('@123aA')
+    cy.get('#inputNewPassword2').type('@123aA')
+    cy.get('[style="width: 304px; height: 78px;"] > div > iframe').click()
+    cy.get('[align="center"] > .btn').click();
+    
   })
 })
